9f0be45a7ccd5fa8ff81b88a5b664a35fe25a6ca
Pavol Almasi <pavol_the_best@hotmail.com>
2017-12-25 23:08:47 -0500

    Ninject Implementation

---
+++ Chapter6-EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Chapter6_EssentialTools.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace Chapter6_EssentialTools.Controllers
+{
+  public class HomeController : Controller
+  {
+    private IValueCalculator calc;
+
+    private Product[] products =
+    {
+      new Product { Name = "Kayak",
+                    Category = "Watersports",
+                    Price = 275m },
+      new Product { Name = "LifeJacket",
+                    Category = "Watersports",
+                    Price = 48.95m },
+      new Product { Name = "Soccer Ball",
+                    Category = "Soccet",
+                    Price = 19.50m },
+      new Product { Name = "Corner Flag",
+                    Category = "Soccer",
+                    Price = 34.95m }
+    };
+
+    public HomeController(IValueCalculator calcParam)
+    {
+      calc = calcParam;
+    }
+
+    // GET: Home
+    public ActionResult Index()
+    {
+      var ninjectKernel = new StandardKernel();
+      ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+
+      IValueCalculator calc = ninjectKernel.Get<IValueCalculator>(); 
+
+      var cart = new ShoppingCart(calc) { Products = products }; //this is the equivalent of the two lines below
+      //var cart = new ShoppingCart(calc);
+      //cart.Products = products;
+
+      decimal totalValue = cart.CalculateProductTotal();
+      return View(totalValue);
+    }
+  }
+}
---
+++ Chapter6-EssentialTools/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,36 @@
+﻿using Chapter6_EssentialTools.Models;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Chapter6_EssentialTools.Infrastructure
+{
+  public class NinjectDependencyResolver : IDependencyResolver
+  {
+    private IKernel kernel;
+
+    public NinjectDependencyResolver(IKernel kernelParam)
+    {
+      kernel = kernelParam;
+      AddBindings();
+    }
+
+    public object GetService(Type serviceType)
+    {
+      return kernel.TryGet(serviceType);
+    }
+
+    public IEnumerable<object> GetServices(Type serviceType)
+    {
+      return kernel.GetAll(serviceType);
+    }
+
+    private void AddBindings()
+    {
+      kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+    }
+  }
+}
